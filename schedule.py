bcb54575005ccd45d9be9cd9965a93877614c868
Daria Sokolova <65014485+PryanyyMed@users.noreply.github.com>
2020-08-03 17:24:58 +0300

    Add files via upload

---
+++ schedule.py
@@ -0,0 +1,22 @@
+#1. Принимаю от пользователя дату
+#2. Составляю расписание раз в 2 дня (через день)
+#3. На 30 дней ДД ММ ГГ и день недели
+#4. Если это воскресенье, то перенести на понеделтник и опять через день
+
+import datetime
+#прошу у пользователся дату
+date_entry = input("Введите дату в формате ДД, ММ, ГГГГ ")
+
+#перевожу в формат даты ДД ММ ГГГГ - если ввести формат по-другому, то программа не запустится
+dt = datetime.datetime.strptime(date_entry, "%d, %m, %Y")
+
+#вывод списка по дням
+#скачать pip DateTimeRange (https://pypi.org/project/DateTimeRange/#get-iterator)
+from datetimerange import DateTimeRange
+# date_End - финальная дата через 30 дней
+date_End = datetime.timedelta(days=30)
+time_range = DateTimeRange(dt, dt + date_End)
+print ("Расписание на месяц: ")
+#список выводится через день (days=2)
+for value in time_range.range(datetime.timedelta(days=2)):
+    print(value.strftime ("%A, %d %B, %Y"))
