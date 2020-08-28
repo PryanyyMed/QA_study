d9bfbaea474e0f1f0cab797ea0a3495feff8e742
Daria Sokolova <65014485+PryanyyMed@users.noreply.github.com>
2020-08-28 17:35:03 +0300

    Update findname.py

--- findname.py
+++ findname.py
@@ -9,7 +9,7 @@ for word in group_1: #для каждого имени в группе 1
     if word == name: #если имя = павел
         counter_1[word] = counter_1.get(word, 0)+1 #то в словаре от имени (ключ) добавляю значение которое равно количеству имени в списке
         max_1 = max(counter_1.values()) #записываю переменную, которая равна максимальному количеству повторений имени Павлов
-        max_list.append(max_1)#добавляю это значение в лист максимальных значений, чтобы потом сравнить с остальными 
+max_list.append(max_1)#добавляю это значение в лист максимальных значений, чтобы потом сравнить с остальными 
         
 
 
@@ -18,14 +18,14 @@ for word in group_2:
     if word == name:
         counter_2[word] = counter_2.get(name, 0)+1
         max_2 = max(counter_2.values())
-        max_list.append(max_2)
+max_list.append(max_2)
 
 counter_3 = {}
 for word in group_3:
     if word == name:
         counter_3[word] = counter_3.get(name, 0)+1
         max_3 = max(counter_3.values())
-        max_list.append(max_3)
+max_list.append(max_3)
 
 max_max = 0
 for i in max_list:
